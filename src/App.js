2ee82a827d5b7638a9ceb2ccc158493795a31c0d
Nitin Misra <nitinmisra@Nitins-MacBook-Pro.local>
2019-07-02 13:11:28 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,38 +3,62 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
-  contsructor(props) {
+  constructor(props) { // misspelled constructor 'contsructor'
+  super(props) // missing super
     this.state = {
       friendsList: ["john", "jacob", "jingle", "heimer", "schmidt"],
       friend: ""
     };
+    
+    // function binds were missing
+    this.changeHandler = this.changeHandler.bind(this)
+    this.addFriend = this.addFriend.bind(this)
+    this.deleteFriend = this.deleteFriend.bind(this)
+    // function binds were missing
+
   }
   changeHandler(event) {
     this.setState({
-      friend: event
+      friend: event.target.value
     });
   }
 
   addFriend() {
-    this.state.friendsList.push(this.state.friend);
+    // this.State.friendsList.push(this.state.friend); incorrect way to set state
+    
+    this.setState({ //proper way to setState, but didnt use a copy of the array
+      friendsList: [...this.state.friendsList, this.state.friend] 
+    })
   }
 
   deleteFriend(index) {
-    this.state.friendsList.splice(index, 1);
-  }
+    // this.State.friendsList.splice(index, 1); incorrect way to set state
+    const myUpdatedList = this.state.friendsList.slice()
+    myUpdatedList.splice(index, 1)
+    this.setState({
+      friendsList: myUpdatedList
+    })
+
+    }
+  
 
   render() {
-    const mappedFreinds = this.state.friendsList.map((friend, index) => {
+  const mappedFreinds = this.state.friendsList.map((friend, index) => {
+    // add return
+    return (
       <div key={index}>
         <span>{friend}</span>
-        <button onClick={this.state.deleteFriend} />
-      </div>;
-    });
+        {/* pass arrow function, invoke delete friend and pass in index */}
+        <button onClick={() => this.deleteFriend(index)}>Delete</button>
+      </div>
+    );
+  });
+
     return (
       <div className="App">
         <div>{mappedFreinds}</div>
         <input
-          type="number"
+          type="text"
           onChange={this.changeHandler}
           value={this.state.friend}
         />
@@ -43,5 +67,4 @@ class App extends Component {
     );
   }
 }
-
 export default App;
